16e4dc2086b3768bcad5117f191b776e500ced57
islom <iabdugofurov99@gmail.com>
2024-04-01 11:05:23 +0500

    local rasmni korsatish uchun

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => (
             <Provider store={store}>
                 <AppThemeProvider>
                     <AppLocaleProvider>
-                        <BrowserRouter >
+                        <BrowserRouter basename={'/'}>
                             <JWTAuthAuthProvider>
                                 <AuthRoutes>
                                     <AppLayout/>
